a924132bf8f38a5ed1795a34d08e347b96e6d26e
mahesh <dananjayamahesh@gmail.com>
2022-04-25 16:11:17 +0000

    config systems: std::to_string is used

--- workload/config/config_system.h
+++ workload/config/config_system.h
@@ -15,9 +15,9 @@ void ModifyComputeNodeConfig(int argc, char* argv[]) {
   	assert(argv[2] != NULL && argv[3] != NULL);
   	//std::cout << std::string(argv[2]) << "  " << std::string(argv[3])  << std::endl;  	
     //std::string s1 = "sed -i '5c \"thread_num_per_machine\": " + std::string(argv[2]) + ",' " + config_file;
-    std::string s1 = "sed -i '5c \"thread_num_per_machine\": " + 16 + ",' " + config_file;
+    std::string s1 = "sed -i '5c \"thread_num_per_machine\": " + std::to_string(16) + ",' " + config_file;
     //std::string s2 = "sed -i '6c \"coroutine_num\": " + std::string(argv[3]) + ",' " + config_file;
-    std::string s2 = "sed -i '6c \"coroutine_num\": " + 8 + ",' " + config_file;
+    std::string s2 = "sed -i '6c \"coroutine_num\": " + std::to_string(8) + ",' " + config_file;
     system(s1.c_str());
     system(s2.c_str());
   }

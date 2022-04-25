f2a612b1adfcd19ba534f6ec2220e84118ac3a6b
mahesh <dananjayamahesh@gmail.com>
2022-04-25 14:54:12 +0000

    add assert

--- workload/config/config_system.h
+++ workload/config/config_system.h
@@ -10,7 +10,8 @@
 void ModifyComputeNodeConfig(int argc, char* argv[]) {
   std::string config_file = "../../../config/compute_node_config.json";
   if (argc == 3) {
-  	assert(argv!=NULL);
+  	assert(argv[2] != NULL && argv[3] != NULL);
+  	std::cout << std::string(argv[2]) << "  " << std::string(argv[3])  << std::endl;
     std::string s1 = "sed -i '5c \"thread_num_per_machine\": " + std::string(argv[2]) + ",' " + config_file;
     std::string s2 = "sed -i '6c \"coroutine_num\": " + std::string(argv[3]) + ",' " + config_file;
     system(s1.c_str());

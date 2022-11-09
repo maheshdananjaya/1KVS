6251fadacb5cb00934ae728fbfcf82fa07654baa
mahesh <dananjayamahesh@gmail.com>
2022-11-09 11:40:11 +0000

    init stat vars

--- workload/stat/result_collect.cc
+++ workload/stat/result_collect.cc
@@ -102,7 +102,7 @@ void InitCounters(node_id_t machine_num, node_id_t machine_id, t_id_t thread_num
   window_start_time = new double[thread_num_per_machine]();
   window_curr_time = new double[thread_num_per_machine]();
 
-  for(i=0;i<thread_num_per_machine;i++){
+  for(int i=0;i<thread_num_per_machine;i++){
     //initial values
     tx_attempted[i] = 0;
     tx_commited[i] = 0;

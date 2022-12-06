5420cb3a973aba9a3bdff0037898da733575e7ae
mahesh <dananjayamahesh@gmail.com>
2022-12-06 15:08:07 +0000

    fix  dtx hrd for new caches

--- include/dtx/dtx.h
+++ include/dtx/dtx.h
@@ -339,7 +339,7 @@ class DTX {
     public: 
      
       bool TxLatchRecovery(coro_yield_t& yield, AddrCache ** addr_caches);
-      bool TxUndoRecovery(coro_yield_t& yield, , AddrCache ** addr_caches);
+      bool TxUndoRecovery(coro_yield_t& yield, AddrCache ** addr_caches);
       bool IssueLatchLogRecoveryRead(coro_yield_t& yield);
       bool IssueUndoLogRecovery(coro_yield_t& yield);
       bool IssueLatchLogRecoveryReadForAllThreads(coro_yield_t& yield, AddrCache ** addr_caches);

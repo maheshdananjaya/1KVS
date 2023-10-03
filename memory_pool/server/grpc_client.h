237900bc582eee01cbd0e63f09e4b69a4bb5f1fa
maheshd <maheshd@localhost>
2023-10-03 11:31:03 -0400

    add protoco and grpc to memory servers

---
+++ memory_pool/server/grpc_client.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#ifdef FD
+#include <grpcpp/grpcpp.h>
+#endif
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+//TODO
+enum ServerState{
+  ACK=0,
+  ACTIVE=1,
+  FAILED=2,
+  RECOVERY=3,
+  RECOVERY_DONE=4,
+  RECONFIGURE=5,
+  RECONF_DONE=6,
+  RESUME=7
+};
+
+class GreeterClient {
+ public:
+  GreeterClient(std::shared_ptr<Channel> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string SayHello(const std::string& user) {
+    // Data we are sending to the server.
+    HelloRequest request;
+    request.set_name(user);
+
+    // Container for the data we expect from the server.
+    HelloReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SayHello(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};

81be65c86fb2976660baada9ff2f444f601831cd
Mahesh <dananjayamahesh@gmail.com>
2024-12-30 22:44:01 +0000

    Create micro.sh
    micro scripts

---
+++ edbt_scripts/micro.sh
@@ -0,0 +1,26 @@
+
+#cd ../
+#rm -rf build
+#bash build.sh
+
+cd ../build/workload/micro
+
+parallel-ssh -i -H node-5 'bash setMICRO.sh'
+parallel-ssh -i -H node-6 'bash setMICRO.sh'
+parallel-ssh -i -H node-6 'bash setMemId.sh'
+
+parallel-ssh -i -H node-5 'bash termexp.sh'
+parallel-ssh -i -H node-6 'bash termexp.sh'
+
+sed -i '5c "thread_num_per_machine": 16,' ../../../config/compute_node_config.json
+sed -i '6c "coroutine_num": 9,' ../../../config/compute_node_config.json
+
+parallel-ssh -i -H node-8 'bash startfd.sh'
+parallel-ssh -i -H node-5 'bash startexp.sh'
+parallel-ssh -i -H node-6 'bash startexp.sh'
+sleep 10
+./micro_bench u-100
+parallel-ssh -i -H node-5 'bash termexp.sh'
+parallel-ssh -i -H node-6 'bash termexp.sh'
+parallel-ssh -i -H node-8 'bash termfd.sh'
+~                                         

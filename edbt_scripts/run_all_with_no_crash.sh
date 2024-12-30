d2406bd0ff147de8d244fe3988ce4e0216005b94
Mahesh <dananjayamahesh@gmail.com>
2024-12-30 22:46:50 +0000

    Create run_all_with_no_crash.sh
    no crash scripts

---
+++ edbt_scripts/run_all_with_no_crash.sh
@@ -0,0 +1,36 @@
+#Experiment
+
+cd ../
+
+
+sed -i '96c #define CRASH_INTERVAL 500000' workload/micro/micro_bench.cc
+sed -i '96c #define CRASH_INTERVAL 750000' workload/smallbank/smallbank_bench.cc
+sed -i '96c #define CRASH_INTERVAL 750000' workload/tatp/tatp_bench.cc
+sed -i '96c #define CRASH_INTERVAL 50000' workload/tpcc/tpcc_bench.cc
+
+
+sed -i '100c //#define CRASH_ENABLE' include/common/common.h
+sed -i '101c //#define NORESUME' include/common/common.h
+sed -i '108c //#define LATCH_STALL' include/common/common.h
+sed -i '109c //#define LATCH_STALL_RECOVERY' include/common/common.h
+
+rm -rf build
+bash build.sh
+cd edbt_scripts
+
+
+for INDEX in 1 2 3 4 5;
+do
+        bash micro.sh
+        bash smallbank.sh
+        bash tatp.sh
+        bash tpcc.sh
+
+        mv ../build/workload/micro/result_all_threads.txt ../edbt_results/micro_no_crash_${INDEX}
+        mv ../build/workload/smallbank/result_all_threads.txt ../edbt_results/smallbank_no_crash_${INDEX}
+        mv ../build/workload/tatp/result_all_threads.txt ../edbt_results/tatp_no_crash_${INDEX}
+        mv ../build/workload/tpcc/result_all_threads.txt ../edbt_results/tpcc_no_crash_${INDEX}
+
+
+done
+#Experiment

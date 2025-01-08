2463c9adf15e09d14d4bb1de74cbc55ce6740c10
Mahesh <dananjayamahesh@gmail.com>
2025-01-08 01:39:54 +0000

    added naive log overhead scripts to edbt

---
+++ edbt_scripts/run_naive_logging.sh
@@ -0,0 +1,58 @@
+#Experiment
+
+cd ../
+
+
+sed -i '96c #define CRASH_INTERVAL 500000' workload/micro/micro_bench.cc
+sed -i '96c #define CRASH_INTERVAL 750000' workload/smallbank/smallbank_bench.cc
+sed -i '96c #define CRASH_INTERVAL 750000' workload/tatp/tatp_bench.cc
+sed -i '96c #define CRASH_INTERVAL 50000' workload/tpcc/tpcc_bench.cc
+
+
+sed -i '100c //#define CRASH_ENABLE' include/common/common.h
+sed -i '101c //#define NORESUME' include/common/common.h
+sed -i '108c //#define LATCH_STALL' include/common/common.h
+sed -i '109c #define LATCH_STALL_RECOVERY' include/common/common.h\
+sed -i '103c //#define MEM_FAILURES' include/common/common.h
+sed -i '104c //#define MEM_CRASH_ENABLE' include/common/common.h
+
+
+for LOGGING in WITH WITHOUT;
+do
+
+sed -i "91c #define ${LOGGING}_LATCH_LOGGING" include/common/common.h
+
+rm -rf build
+bash build.sh
+cd edbt_scripts
+
+
+for INDEX in 1 2 3 4 5;
+do
+        bash micro.sh
+        bash smallbank.sh
+        bash tatp.sh
+        bash tpcc.sh
+
+        bash run_extract_compute.sh
+
+        mv ../build/workload/micro/result_all_threads.txt ../edbt_results/micro_${LOGGING}_naive_log_${INDEX}
+        mv ../build/workload/smallbank/result_all_threads.txt ../edbt_results/smallbank_${LOGGING}_naive_log_${INDEX}
+        mv ../build/workload/tatp/result_all_threads.txt ../edbt_results/tatp_${LOGGING}_naive_log_${INDEX}
+        mv ../build/workload/tpcc/result_all_threads.txt ../edbt_results/tpcc_${LOGGING}_naive_log_${INDEX}
+
+
+done
+
+bash average.sh ../edbt_results/micro_${LOGGING}_naive_log 5
+bash average.sh ../edbt_results/smallbank_${LOGGING}_naive_log 5
+bash average.sh ../edbt_results/tatp_${LOGGING}_naive_log 5
+bash average.sh ../edbt_results/tpcc_${LOGGING}_naive_log 5
+
+cd ..
+
+done
+
+sed -i "91c //#define $WITH_LATCH_LOGGING" include/common/common.h
+
+#Experiment
